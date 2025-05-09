752ce1204045f65e4c110895867a9e142cdfa0e4
Levy Atreus <levyatreus@gmail.com>
2025-05-09 11:34:51 -0300

    exercicio completo

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,22 +1,69 @@
+import produtosPage from "../support/page_objects/produtos.page.js";
 /// <reference types="cypress" />
 
+let dadosLogin;
+
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-  /*  Como cliente 
-      Quero acessar a Loja EBAC 
-      Para fazer um pedido de 4 produtos 
-      Fazendo a escolha dos produtos
-      Adicionando ao carrinho
-      Preenchendo todas opções no checkout
-      E validando minha compra ao final */
+    before(() => {
+        cy.fixture('perfil').then(perfil => {
+            dadosLogin = perfil;
+        });
+    });
 
-  beforeEach(() => {
-      cy.visit('/')
-  });
+    beforeEach(() => {
+        cy.visit('minha-conta');
+        cy.get('#username').type(dadosLogin.usuario);
+        cy.get('#password').type(dadosLogin.senha);
+        cy.get('.woocommerce-form > .button').click();
+        cy.get('.page-title').should('contain', 'Minha conta');
+    });
 
-  it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
-  });
+    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
+        cy.fixture('produtos').then(dadosProdutos => {
+            // Adiciona o primeiro produto ao carrinho
+            produtosPage.buscarProduto(dadosProdutos[1].nomeProduto);
+            produtosPage.addProdutoCarrinho(
+                dadosProdutos[1].tamanho,
+                dadosProdutos[1].cor,
+                dadosProdutos[1].quantidade
+            );
+            cy.get('.woocommerce-message > .button').click(); // Vai para o carrinho ou continua comprando
 
+            // Adiciona o segundo produto ao carrinho
+            produtosPage.buscarProduto(dadosProdutos[2].nomeProduto);
+            produtosPage.addProdutoCarrinho(
+                dadosProdutos[2].tamanho,
+                dadosProdutos[2].cor,
+                dadosProdutos[2].quantidade
+            );
+            cy.get('.woocommerce-message > .button').click(); // Vai para o carrinho ou continua comprando
 
-})
+            // Adiciona o terceiro produto ao carrinho
+            produtosPage.buscarProduto(dadosProdutos[3].nomeProduto);
+            produtosPage.addProdutoCarrinho(
+                dadosProdutos[3].tamanho,
+                dadosProdutos[3].cor,
+                dadosProdutos[3].quantidade
+            );
+            cy.get('.woocommerce-message > .button').click(); // Vai para o carrinho ou continua comprando
+
+            // Adiciona o quarto produto (você estava repetindo o terceiro)
+            produtosPage.buscarProduto(dadosProdutos[0].nomeProduto); // Assumindo que você queria o índice 0 para o quarto produto
+            produtosPage.addProdutoCarrinho(
+                dadosProdutos[0].tamanho,
+                dadosProdutos[0].cor,
+                dadosProdutos[0].quantidade
+            );
+            cy.get('.woocommerce-message > .button').click(); // Vai para o carrinho
+
+            // Segue para o checkout (agora deve ser chamado apenas uma vez)
+            cy.get('.checkout-button').first().click();
+
+            // TODO: Preencher os dados do checkout
+            // ...
+
+            // TODO: Validar a compra ao final
+            // ...
+        });
+    });
+});
--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -1,6 +1,7 @@
 import produtosPage from "../support/page_objects/produtos.page.js";
 //funcionando
-describe('Funcionalidade: Produtos', () => {
+describe('Funcionalidade: Produtos', () => 
+{
 
   beforeEach(() => {
     cy.visit('produtos');
@@ -8,7 +9,7 @@ describe('Funcionalidade: Produtos', () => {
   });
 
   it('Deve selecionar um produto da lista', () => {
-    cy.get('.product-block:contains("Aero Daily Fitness Tee")')
+    cy.get('.product-block:contains("Josie Yoga Jacket")')
       .find('h3.name a')
       .click();
     cy.get('#tab-title-description > a').should('contain', 'Descrição');
@@ -16,21 +17,21 @@ describe('Funcionalidade: Produtos', () => {
   });
 
   it('Deve buscar um produto com sucesso', () => {
-    let produto = 'Aero Daily Fitness Tee'
+    let produto = 'Josie Yoga Jacket'
     produtosPage.buscarProduto(produto);
     cy.get('.product_title').should('contain', produto)
     // funcionando
   });
 
   it('Deve visitar a página do produto', () => {
-    produtosPage.visitarProduto('Aero Daily Fitness Tee')
-    cy.get('.product_title').should('contain', 'Aero Daily Fitness Tee')
+    produtosPage.visitarProduto('Josie Yoga Jacket')
+    cy.get('.product_title').should('contain', 'Josie Yoga Jacket')
     // funcionando
   });
 
   it('Deve adicionar produto ao carrinho', () => {
     let qtd = 1
-    produtosPage.buscarProduto('Aero Daily Fitness Tee')
+    produtosPage.buscarProduto('Josie Yoga Jacket')
     produtosPage.addProdutoCarrinho('M', 'Black', qtd)
     // funcionando
   });
@@ -45,7 +46,47 @@ describe('Funcionalidade: Produtos', () => {
       dados[1].quantidade)
 
     cy.get('.product_title').should('contain', dados[1].nomeProduto)
+    cy.get('.single_add_to_cart_button').click()
+    cy.get('.woocommerce-message > .button').click()
     // funcionando
-   });
+    });
+  });
+
+  it('deve acicionar o produto ao carrinho de forma simples', () => {
+    //produtosPage.buscarProduto('Josie Yoga Jacket')
+    
+    cy.get('.button-variable-item-S').click()
+    cy.get('.button-variable-item-Blue').click()
+    cy.get('.input-text').clear().type('1')
+
+    cy.get('.single_add_to_cart_button').click()
+    cy.get('.woocommerce-message > .button').click()
+    cy.get('.checkout-button').click()
+  });
+
+  describe('Fluxo de compra com login', () => {
+    it.only('Deve adicionar produto ao carrinho com massa de dados após o login', () => {
+      cy.fixture('produtos').then(dados => {
+        // **Passos de Login**
+        cy.visit('/minha-conta'); // Ou a URL da sua página de login
+        cy.get('#username').type('seu_usuario'); // Seletor do campo de usuário
+        cy.get('#password').type('sua_senha'); // Seletor do campo de senha
+        cy.get('button[name="login"]').click(); // Seletor do botão de login
+        cy.url().should('not.include', '/minha-conta'); // Asserção para verificar o login (redirecionamento)
+  
+        // **Passos para adicionar o produto ao carrinho (seu código existente)**
+        produtosPage.buscarProduto(dados[1].nomeProduto)
+        produtosPage.addProdutoCarrinho(
+          dados[1].tamanho,
+          dados[1].cor,
+          dados[1].quantidade
+        )
+  
+        cy.get('.product_title').should('contain', dados[1].nomeProduto)
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.woocommerce-message > .button').click()
+        // funcionando
+      });
+    });
   });
 });
